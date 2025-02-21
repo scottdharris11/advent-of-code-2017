340ef6dbccebc2d91a87f081ce73548755c25f8d
scottdharris <scottdharris11@yahoo.com>
2025-02-20 19:27:33 -0500

    day 13 part 1

--- day13.py
+++ day13.py
@@ -5,20 +5,33 @@ from utilities.runner import runner
 @runner("Day 13", "Part 1")
 def solve_part1(lines: list[str]) -> int:
     """part 1 solving function"""
-    return len(lines)
+    severity = 0
+    for line in lines:
+        depth, size = (int(x) for x in line.split(": "))
+        if caught(depth, size):
+            severity += depth * size
+    return severity
 
 @runner("Day 13", "Part 2")
 def solve_part2(lines: list[str]) -> int:
     """part 2 solving function"""
     return len(lines)
 
+def caught(pico: int, size: int) -> bool:
+    """determine if the scanner will catch you"""
+    every = (size - 1) * 2
+    return pico % every == 0
+
 # Data
 data = read_lines("input/day13/input.txt")
-sample = """""".splitlines()
+sample = """0: 3
+1: 2
+4: 4
+6: 4""".splitlines()
 
 # Part 1
-assert solve_part1(sample) == 0
-assert solve_part1(data) == 0
+assert solve_part1(sample) == 24
+assert solve_part1(data) == 3184
 
 # Part 2
 assert solve_part2(sample) == 0
