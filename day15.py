e347a2e96d340cd70931e6ace88de963d1d00ef5
scottdharris <scottdharris11@yahoo.com>
2025-02-21 13:21:59 -0500

    day 15 setup

---
+++ day15.py
@@ -0,0 +1,20 @@
+"""utility imports"""
+from utilities.runner import runner
+
+@runner("Day 15", "Part 1")
+def solve_part1(aseed: int, bseed: int) -> int:
+    """part 1 solving function"""
+    return 0
+
+@runner("Day 15", "Part 2")
+def solve_part2(aseed: int, bseed: int) -> int:
+    """part 2 solving function"""
+    return 0
+
+# Part 1
+assert solve_part1(65, 8921) == 0
+assert solve_part1(873, 583) == 0
+
+# Part 2
+assert solve_part2(65, 8921) == 0
+assert solve_part2(873, 583) == 0
