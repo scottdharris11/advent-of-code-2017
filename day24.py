1afbe71a96e1f3a0daead1a05ef5041e5eeedcaf
scottdharris <scottdharris11@yahoo.com>
2025-03-07 14:04:08 -0500

    day 24, part 2

--- day24.py
+++ day24.py
@@ -5,13 +5,7 @@ from utilities.runner import runner
 @runner("Day 24", "Part 1")
 def solve_part1(lines: list[str]) -> int:
     """part 1 solving function"""
-    magnets = {}
-    for i, line in enumerate(lines):
-        m = Magnet(i, line)
-        for p in m.pins:
-            avail = magnets.get(p, [])
-            avail.append(m)
-            magnets[p] = avail
+    magnets, _ = parse_magnets(lines)
     max_strength = 0
     for zero in magnets[0]:
         strength = max_chain_strength(zero, 0, magnets, set())
@@ -22,7 +16,17 @@ def solve_part1(lines: list[str]) -> int:
 @runner("Day 24", "Part 2")
 def solve_part2(lines: list[str]) -> int:
     """part 2 solving function"""
-    return 0
+    magnets, by_id = parse_magnets(lines)
+    longest = []
+    for zero in magnets[0]:
+        longest = longest_chains(zero, 0, [], magnets, longest)
+    best = 0
+    for chain in longest:
+        strength = 0
+        for m in chain:
+            strength += by_id[m].strength
+        best = max(best, strength)
+    return best
 
 class Magnet:
     """defines magnet structure"""
@@ -33,6 +37,21 @@ class Magnet:
 
     def __repr__(self) -> str:
         return str((self.uid, self.strength, self.pins))
+
+def parse_magnets(lines: list[str]) -> tuple[dict[int,list[Magnet]],dict[int,Magnet]]:
+    """parse magnets from input"""
+    by_pins = {}
+    by_id = {}
+    for i, line in enumerate(lines):
+        m = Magnet(i, line)
+        by_id[i] = m
+        for p in m.pins:
+            avail = by_pins.get(p, [])
+            if m in avail:
+                continue
+            avail.append(m)
+            by_pins[p] = avail
+    return by_pins, by_id
 
 def max_chain_strength(m: Magnet, prev: int, magnets: dict[int,list[Magnet]], used: set[int]) -> int:
     """find the maximum chain strength for the supplied magnet"""
@@ -50,6 +69,28 @@ def max_chain_strength(m: Magnet, prev: int, magnets: dict[int,list[Magnet]], us
     used.remove(m.uid)
     return m.strength + max_next
 
+def longest_chains(m: Magnet, prev: int, chain: list[int], magnets: dict[int,list[Magnet]], chains: list[tuple[int]]) -> list[list[int]]:
+    """find the maximum chain strength for the supplied magnet"""
+    chain.append(m.uid)
+    n = m.pins[0]
+    if n == prev:
+        n = m.pins[1]
+    end = True
+    for magnet in magnets[n]:
+        if magnet.uid in chain:
+            continue
+        end = False
+        chains = longest_chains(magnet, n, chain, magnets, chains)
+    if end:
+        c = tuple(chain)
+        if len(chains) == 0 or len(c) == len(chains[0]):
+            chains.append(c)
+        elif len(c) > len(chains[0]):
+            chains = []
+            chains.append(c)
+    chain.pop()
+    return chains
+
 # Data
 data = read_lines("input/day24/input.txt")
 sample = """0/2
@@ -66,5 +107,5 @@ assert solve_part1(sample) == 31
 assert solve_part1(data) == 1859
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample) == 19
+assert solve_part2(data) == 1799
